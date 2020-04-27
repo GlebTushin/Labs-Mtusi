659c5454e9db4bd13ce6f0a51c3c1e16bdf53cbf
GlebTushin <g.tushin2016@gmail.com>
2020-04-28 00:37:32 +0300

    изменения

--- Zadachi/Zadachi4.6/src/t6.java
+++ Zadachi/Zadachi4.6/src/t6.java
@@ -2,8 +2,9 @@ public class t6 {public static int mult (int num){
    int count=0;
    int num1=1;
     while (num>9){
-        for (int i=0;i<String.valueOf(num).length();i++){
-            num1=num1*(int)(String.valueOf(num).charAt(i));
+        for (int i=0;i<String.valueOf(num).length()+1;i++){
+            num1=num1*(num%10);
+            num=num/10;
         }
        num=num1;
         count=count+1;
